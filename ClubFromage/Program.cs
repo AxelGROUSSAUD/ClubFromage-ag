8b2e4ebf7e0de9148ccc5e3cb743a7afc480a51f
Axel Groussaud <axel.groussaud@saintmichelannecy.fr>
2020-09-11 11:57:55 +0200

    first commit

---
+++ ClubFromage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubFromage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBAL clubfromage = new DBAL();
+            string requetesql;
+            
+            //clubfromage.Insert(requetesql);
+            requetesql = "SELECT * FROM Pays;";
+            //clubfromage.Select(requetesql);
+            requetesql = "UPDATE Pays SET nom = 'Brezil' WHERE id=1 ;";
+            //clubfromage.Update(requetesql);
+            requetesql = "DELETE FROM Pays WHERE id = 3;";
+            //clubfromage.Delete(requetesql);
+
+            requetesql = "DELETE FROM Pays WHERE id = 25;";
+            clubfromage.ExecQuery(requetesql);
+        }
+    }
+}
